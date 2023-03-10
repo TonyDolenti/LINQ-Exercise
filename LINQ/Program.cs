e731c40752a5ca98a3c609d380304fffd5b77a05
TonyDolenti <antoniobdolenti@gmail.com>
2023-03-10 14:19:18 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LINQ
+﻿using System.Linq;
+namespace LINQ
 {
     public class Program
     {
+        
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Modern Warfare 19", "Black Ops", "Batman Arkham Knight" };
+            videoGames.Add("World At War");
+            videoGames.Add("Assassins Creed 3");
+
+            var orderedGames = videoGames.OrderBy(title => title.Length);
+            foreach (var games in orderedGames)
+            {
+                Console.WriteLine(games);
+            }
         }
     }
 }
